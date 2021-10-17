2595a3a66aeeb8c24527a7b8655425405b3bbe37
Branislav Stojčić <82265527+BaneProjects@users.noreply.github.com>
2021-10-17 15:56:06 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,14 +1,15 @@
+
 const themes = document.getElementsByClassName("theme");
 for (let i = 0; i < themes.length; i++) {
   themes[i].addEventListener("click", (el) => {
     var colorTheme = el.target.id;
-    document.documentElement.setAttribute("data-theme", colorTheme);
 
+    document.documentElement.setAttribute("data-theme", colorTheme);
     for (let i = 0; i < themes.length; i++) {
-      themes[i].style.setProperty("background-color", "inherit");
+      themes[i].style.opacity = "0";
     }
 
     var btn = document.getElementById(colorTheme);
-    btn.style.setProperty("background-color", "var(--bg)");
+    btn.style.opacity = "1";
   });
 }
